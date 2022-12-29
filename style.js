321d55882ef29389c01520f3115b1a51cf515823
Shubham1032 <sharmashubham19298@gmail.com>
2022-12-29 14:14:52 -0500

    updated

--- style.js
+++ style.js
@@ -10,6 +10,8 @@ let timezone=document.getElementById("timezone");
 let grab=document.getElementById("grab");
 let Good=document.getElementById("Good");
 let imagepg=document.getElementById("imagepg");
+let forcomp=new Date().getHours();
+
 
 function clock(){
     let timer=new Date();
@@ -23,30 +25,28 @@ function clock(){
     hourElement.innerText=`${hours}     hours`;
     if(hours>=12 && hours<16){
         timezone.innerText="PM";
-        Good.innerText="GOOD AFTERNOON!! TAKE SOME SLEEP";
-        grab.innerText="LET'S HAVE SOME LUNCH !!";
+      
 
-       imagepg.style.backgroundImage="URL('./EVENINGiMAGE.png')";
+    //    imagepg.style.backgroundImage="URL('./EVENINGiMAGE.png')";
     }
    else if(hours>=16 && hours<20){
     timezone.innerText="PM";
-        Good.innerText="GOOD EVENING !!";
-        grab.innerText="STOP YAWNING,GET SOME TEA.. ITS JUST EVENING!";
-       imagepg.style.backgroundImage="URL('./lunch_image.png')";
+       
+    //    imagepg.style.backgroundImage="URL('./lunch_image.png')";
     }
  else  if(hours>=20 && hours<=24){
     timezone.innerText="PM";
-        Good.innerText="GOOD NIGHT !!";
-        grab.innerText="CLOSE YOUR EYES AND GO TO SLEEP";
-        imagepg.style.backgroundImage="URL('./NIGHTIMAGE.png')";
+      
+        // imagepg.style.backgroundImage="URL('./NIGHTIMAGE.png')";
     };
     
 // to not display time in 24 hour format
  function hoursdisplay(){
-    if(hours>=12){
+    if(hours>12){
         let newhours=hours-12;
         hourElement.innerText=`${newhours}   hours`;
     }
+    // forcomp=hours;
  }
  hoursdisplay();
 }
@@ -56,26 +56,42 @@ setInterval(clock,1000);
 
 
 let selectwakeup=document.getElementById("selectwakeup");
+
 let selectlunch=document.getElementById("selectlunch");
+let lunchvalues;
 let selectnap=document.getElementById("selectnap");
+let napvalues;
+
 let selectnight=document.getElementById("selectnight");
+let nightvalues;
 let wakeup;
 let lunchtext;
 let naptime;
 let nighttime;
-
+let wakeupvalues;
 selectwakeup.addEventListener("change",function(event){
     wakeup=`Wake UP Time:${(event.target.selectedOptions[0].text)}`;
+    wakeupvalues=event.target.selectedOptions[0].value;
+    
 })
+console.log(wakeupvalues)
 selectlunch.addEventListener("change",function(event){
-    lunchtime=`Lunch Time :${(event.target.selectedOptions[0].text)}`;
+    
+    lunchtext=`Lunch Time :${(event.target.selectedOptions[0].text)}`;
+    lunchvalues=event.target.selectedOptions[0].value;
 })
+
 selectnap.addEventListener("change",function(event){
     naptime=`Nap Time:${(event.target.selectedOptions[0].text)}`;
+    napvalues=event.target.selectedOptions[0].value;
+    
+   
 })
 selectnight.addEventListener("change",function(event){
     nighttime=`Night Time :${(event.target.selectedOptions[0].text)}`;
+    nightvalues=event.target.selectedOptions[0].value;
 })
+console.log(nightvalues)
 
 // to get text from select by set alarm button
 let setAlarmbtn=document.getElementById("setAlarm");
@@ -85,10 +101,48 @@ setAlarmbtn.addEventListener("mouseover",function(){
 setAlarmbtn.addEventListener("mouseleave", function(){
     setAlarm.innerText="SET ALARM ";
 })
+
+ ///////////////// -->   set alarm button       <-- \\\\\\\\\\\\
+
 setAlarmbtn.addEventListener("click", function(){
+  
     wakeupsummry.innerText=wakeup;
-    lunchsummry.innerText=lunchtime;
+  
+    lunchsummry.innerText=lunchtext;
     napsummry.innerText=naptime;
     nightsummry.innerText=nighttime;
+   // console.log(timezone.innerText)
+   if(forcomp>12){
+    forcomp-=12
+}
+   console.log(forcomp);
+   console.log(wakeupvalues);
+   console.log(lunchvalues);
+   console.log(timezone.innerText)
+    if(wakeupvalues==forcomp && timezone.innerText=="PM" || wakeupvalues==forcomp && timezone.innerText=="AM"){
+        imagepg.style.backgroundImage="URL('./MORINGIMAGE.png')";
+        Good.innerHTML="GOOD MORNING!! ";
+        grab.innerHTML="HAVE SOME BREAKFAST !!";
+    }
+   
+    else if(lunchvalues==forcomp && timezone.innerText=="AM" || lunchvalues==forcomp && timezone.innerText=="PM"){
+        imagepg.style.backgroundImage="URL('./lunch_image.png')";
+        Good.innerHTML="GOOD AFTERNOON!! TAKE SOME SLEEP";
+        grab.innerHTML="LET'S HAVE SOME LUNCH !!";
 
+    }
+   
+    else if(napvalues==forcomp && timezone.innerText=="AM" || napvalues==forcomp && timezone.innerText=="PM"){
+        imagepg.style.backgroundImage="URL('./EVENINGiMAGE.png')";
+        Good.innerText="GOOD EVENING !!";
+        grab.innerText="STOP YAWNING,GET SOME TEA.. ITS JUST EVENING!";
+    }
+   
+    else if(nightvalues==forcomp && timezone.innerText=="AM" || nightvalues==forcomp && timezone.innerText=="PM"){
+        imagepg.style.backgroundImage="URL('./NIGHTIMAGE.png')";
+        Good.innerText="GOOD NIGHT !!";
+        grab.innerText="CLOSE YOUR EYES AND GO TO SLEEP";
+    }
+   
+    
 })
